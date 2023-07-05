c4c9c1986d5b180a4a21fa28d34797696477e7cb
Aneesh <aneeshad2@gmail.com>
2023-07-05 11:03:47 +0530

    g

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const menuIcon = document.getElementById("menuIcon");
+
+menuIcon.addEventListener("click", () => {
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("d-none");
+    });
